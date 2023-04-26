c6a681a43b405973deecf388a50c9ed6d9f99e0a
said026 <said026@gmail.com>
2023-04-26 17:31:52 +0200

    chore(navitia): add first api call

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,55 @@
-use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
+use actix_web::{get, App, HttpResponse, HttpServer, Responder};
+use config::Config;
+use http::header::AUTHORIZATION;
+use once_cell::sync::Lazy;
+use std::collections::HashMap;
+
+static CONFIG: Lazy<Config> = Lazy::new(|| {
+    Config::builder()
+        .add_source(config::File::with_name("src/Settings.toml"))
+        .build()
+        .unwrap()
+});
 
 #[get("/")]
 async fn hello() -> impl Responder {
     HttpResponse::Ok().body("Hello world!")
 }
 
-#[post("/echo")]
-async fn echo(req_body: String) -> impl Responder {
-    HttpResponse::Ok().body(req_body)
-}
+#[get("/navitia")]
+async fn navitia() -> HttpResponse {
+    let api_key = CONFIG
+        .to_owned()
+        .try_deserialize::<HashMap<String, String>>()
+        .unwrap()
+        .get("api_key")
+        .unwrap()
+        .clone();
 
-async fn manual_hello() -> impl Responder {
-    HttpResponse::Ok().body("Hey there!")
+    // create client
+    let client = awc::Client::default();
+
+    // construct request
+    let req = client
+        .get("https://api.navitia.io/v1/coverage/fr-idf/journeys?from=stop_area%3AIDFM%3A68105&to=stop_area%3AIDFM%3A71673&")
+        .insert_header((AUTHORIZATION, api_key));
+
+    log::info!("starting HTTP server to navitia");
+
+    // send request and await response
+    let mut res = req.send().await.unwrap();
+    println!("Response: {:?}", res);
+
+    let payload = res.body().await.unwrap();
+
+    HttpResponse::Ok().body(payload)
 }
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| {
-        App::new()
-            .service(hello)
-            .service(echo)
-            .route("/hey", web::get().to(manual_hello))
-    })
-    .bind(("127.0.0.1", 8080))?
-    .run()
-    .await
+    HttpServer::new(|| App::new().service(navitia))
+        .bind(("127.0.0.1", 8080))?
+        .workers(1)
+        .run()
+        .await
 }
